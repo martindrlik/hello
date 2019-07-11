9d953385c46137f4d29a9c1e516738c37104506f
Martin Drlík <martindrlik@hotmail.com>
2019-07-11 23:43:28 +0200

    write output to file

--- hello.go
+++ hello.go
@@ -5,13 +5,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
 var (
 	con = flag.Int("con", 5, "number of concurrent clients repeating request")
 	hld = flag.Duration("hold-for", time.Second, "hold for time")
+	out = flag.String("out", "", "output file name")
 	url = flag.String("url", "", "")
+)
+
+var (
+	rch = make(chan record)
+	w   = os.Stdout
 )
 
 func main() {
@@ -20,11 +27,18 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	rch := make(chan record)
+	if *out != "" {
+		f, err := os.Create(*out)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+		w = f
+	}
 	for i := 0; i < *con; i++ {
 		go func() {
 			for {
-				do(req, rch)
+				do(req)
 			}
 		}()
 	}
@@ -32,14 +46,14 @@ func main() {
 	for {
 		select {
 		case r := <-rch:
-			fmt.Println(r.sc, r.dur)
+			fmt.Fprintln(w, r.sc, r.dur)
 		case <-done:
 			return
 		}
 	}
 }
 
-func do(req *http.Request, rch chan<- record) {
+func do(req *http.Request) {
 	start := time.Now()
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
